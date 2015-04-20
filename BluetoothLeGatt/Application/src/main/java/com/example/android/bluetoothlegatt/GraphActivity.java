fdfd283660626578328b0cca8b75ef688ab8c3a5
Roman <filiroman.tsu@gmail.com>
2015-04-20 19:36:21 +0300

    GraphView added with static info Volley added as a library

---
+++ BluetoothLeGatt/Application/src/main/java/com/example/android/bluetoothlegatt/GraphActivity.java
@@ -0,0 +1,55 @@
+package com.example.android.bluetoothlegatt;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+
+public class GraphActivity extends Activity {
+
+    private LineGraphSeries<DataPoint> mSeries;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        mSeries = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 2),
+                new DataPoint(2, -2),
+                new DataPoint(3, 3),
+                new DataPoint(4, -3)
+        });
+        graph.addSeries(mSeries);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_graph, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
